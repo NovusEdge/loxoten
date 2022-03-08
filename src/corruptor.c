08963dbcf2599bcd2d72066bed68884b09321c5d
NovusEdge <novusedge0@gmail.com>
2022-03-07 17:52:36 -0800

    Adding initial files for the CLI tool

---
+++ src/corruptor.c
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+
+#include "corruptor.h"
+#include "errors.h"
+
+unsigned char *generate_random_bytes (size_t num_bytes);
+
+int main(int argc, char const *argv[]) {
+
+    return 0;
+}
