40c9bca2c0c7a057189d7f11e07ec5e9a0f7d8f0
NovusEdge <novusedge0@gmail.com>
2022-03-08 16:30:19 -0800

    Adding fancy console output and removing unwanted/nonfunctional flags

--- include/corruptor.h
+++ include/corruptor.h
@@ -1,6 +1,9 @@
 #ifndef CORRUPTOR_H
 #define CORRUPTOR_H "corruptor.h"
 
+// version for the project
+#define VERSION     "v1.0.0"
+
 const char* __USAGE_HELP = "USAGE: \n\
     corruptor [flags] [files]";
 
@@ -8,25 +11,21 @@ const char* __OPTIONS = "OPTIONS: \n\
     -V  --version       Print the version corruptor-cli \n\
     -v  --verbose       Use verbose output\n\
     -q  --quiet         Use quiet mode to suppress output.\n\
-                        Note that this option also supresses error messages\n\
-                        to show error messages, use --show-errors\n\
-                                                                 \n\
     -h  --help          Prints help information\n\
-        --show-errors   Makes errors visible even if -q option is provided.\n";
+    -n --num-bytes      Specifies the number of bytes to write to the given\n\
+                        files.\n";
 
 
 const char* FULL_HELP = "USAGE: \n\
     corruptor [flags] [files]\n\
-    \n\
+                             \n\
     OPTIONS: \n\
         -V  --version       Print the version corruptor-cli \n\
         -v  --verbose       Use verbose output\n\
         -q  --quiet         Use quiet mode to suppress output.\n\
-                            Note that this option also supresses error messages\n\
-                            to show error messages, use --show-errors\n\
-                                                                     \n\
         -h  --help          Prints help information\n\
-            --show-errors   Makes errors visible even if -q option is provided.\n";
+        -n --num-bytes      Specifies the number of bytes to write to the given\n\
+                            files.\n";
 
 
 const char* VALID_FLAGS[] = {
@@ -34,7 +33,7 @@ const char* VALID_FLAGS[] = {
     "-v", "--verbose",
     "-q", "--quiet",
     "-h", "--help",
-    "--show-errors",
+    "-n", "--num-bytes",
 };
 
 #endif // corruptor.h
